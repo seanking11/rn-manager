3af9f3d82b936a3df33f07f2bc89a9482d539082
Sean King <kingsc@miamioh.edu>
2017-10-07 19:21:37 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import firebase from 'firebase'
+import reducers from './reducers'
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyBhOSqIW9vUpTJraCFEMGWXJAj3bigqqtA',
+      authDomain: 'rn-manager-cd595.firebaseapp.com',
+      databaseURL: 'https://rn-manager-cd595.firebaseio.com',
+      projectId: 'rn-manager-cd595',
+      storageBucket: 'rn-manager-cd595.appspot.com',
+      messagingSenderId: '1005333706368'
+    }
+
+    firebase.initializeApp(config)
+  }
+
+  render() {
+    return(
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>
+            Hi!
+          </Text>
+        </View>
+      </Provider>
+    )
+  }
+}
+
+export default App
