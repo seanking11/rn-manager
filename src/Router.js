970ab620e0fb28f326e7623a96037527435286a9
Sean King <kingsc@miamioh.edu>
2017-10-17 01:34:06 -0400

    feat(employeeCreate): Add employeeCreate scene

--- src/Router.js
+++ src/Router.js
@@ -1,22 +1,33 @@
 import React from 'react'
-import { Scene, Router } from 'react-native-router-flux'
+import { Scene, Router, Actions } from 'react-native-router-flux'
 import LoginForm from './components/LoginForm'
 import EmployeeList from './components/EmployeeList'
+import EmployeeCreate from './components/EmployeeCreate'
 
 const RouterComponent = () => (
   <Router>
     <Scene key="root" hideNavBar>
       <Scene key="auth">
-        <Scene key="login" component={LoginForm} title="Login" />
+        <Scene
+          key="login"
+          component={LoginForm}
+          title="Login"
+        />
       </Scene>
 
       <Scene key="main">
         <Scene
+          initial
           key="employeeList"
           component={EmployeeList}
           title="Employees"
           rightTitle="Add"
-          onRight={() => console.log('hi')}
+          onRight={() => Actions.employeeCreate()}
+        />
+        <Scene
+          key="employeeCreate"
+          component={EmployeeCreate}
+          title="Create Employee"
         />
       </Scene>
     </Scene>
