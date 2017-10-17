fe66e1b39fd70cf0590f1f5cdfd16219d0afaa51
Sean King <kingsc@miamioh.edu>
2017-10-17 00:49:44 -0400

    feat(EmployeeList): Add second EmployeeList screen

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import { Scene, Router } from 'react-native-router-flux'
 import LoginForm from './components/LoginForm'
+import EmployeeList from './components/EmployeeList'
 
 const RouterComponent = () => (
   <Router>
-    <Scene key="root">
-      <Scene key="login" component={LoginForm} title="Login" />
+    <Scene key="root" hideNavBar>
+      <Scene key="auth">
+        <Scene key="login" component={LoginForm} title="Login" />
+      </Scene>
+
+      <Scene key="main">
+        <Scene
+          key="employeeList"
+          component={EmployeeList}
+          title="Employees"
+          rightTitle="Add"
+          onRight={() => console.log('hi')}
+        />
+      </Scene>
     </Scene>
   </Router>
 )
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase'
+import { Actions } from 'react-native-router-flux'
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
@@ -41,4 +42,6 @@ const loginUserSuccess = (dispatch, user) => {
     type: LOGIN_USER_SUCCESS,
     user
   })
+
+  Actions.main()
 }
