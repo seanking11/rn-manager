4fcdd49bb89836f1a44a90327c252f89083bb2fd
Sean King <kingsc@miamioh.edu>
2017-10-20 18:12:58 -0400

    fix(EmployeeList): Adjust typos

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import React, { Component } from 'react'
 import { ListView, View, Text } from 'react-native'
 import { connect } from 'react-redux'
@@ -6,7 +7,7 @@ import { employeesFetch } from '../actions'
 class EmployeeList extends Component {
   componentWillMount() {
     this.props.employeesFetch()
-    this.createDataSource(this.propss)
+    this.createDataSource(this.props)
   }
 
   componentWillReceiveProps(newProps) {
@@ -14,7 +15,7 @@ class EmployeeList extends Component {
   }
 
   createDataSource({ employees }) {
-    const ds = new ListView.dataSource({
+    const ds = new ListView.DataSource({
       rowHasChanged: (r1, r2) => r1 !== r2
     })
 
@@ -36,4 +37,12 @@ class EmployeeList extends Component {
   }
 }
 
-export default connect(null, { employeeFetch })(EmployeeList)
+const mapStateToProps = state => {
+  const employees = _.map(state.employees, (val, uid) => {
+    return { ...val, uid }
+  })
+
+    return { employees }
+}
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList)
