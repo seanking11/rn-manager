2e44d3dff0e2b1e46ee39debcafecb0c20f46a34
Sean King <kingsc@miamioh.edu>
2017-10-22 23:20:59 -0400

    feat(EmployeeActions): Add employeeSave action

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -31,3 +31,13 @@ export const employeesFetch = () => {
       })
   }
 }
+
+export const employeeSave = ({ name, phone, shift, uid }) => {
+  const { currentUser } = firebase.auth()
+
+  return () => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+      .set({ name, phone, shift })
+      .then(() => Actions.pop())
+  }
+}
